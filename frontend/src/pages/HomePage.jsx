131d559b5379e85d0b35246a72111684f4932b6c
Slok9931 <148342312+Slok9931@users.noreply.github.com>
2024-11-17 13:34:41 +0530

    UI Design Completed

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Search from '../components/Search'
+import SortRepos from '../components/SortRepos'
+import ProfileInfo from '../components/ProfileInfo'
+import Repos from '../components/Repos'
+
+const HomePage = () => {
+  return (
+    <div className='m-4'>
+      <Search />
+      <SortRepos />
+      <div className='flex flex-col gap-4 justify-center items-start lg:flex-row'>
+        <ProfileInfo />
+        <Repos />
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
